b285d84e88c46feda4c49bbca64763430b354e8e
ihsandamar <44813158+ihsandamar@users.noreply.github.com>
2024-10-26 21:51:27 +0300

    fix: sql is able to run on db and db_chain return results

--- DatabaseSchema.py
+++ DatabaseSchema.py
@@ -1,12 +1,11 @@
 from sqlalchemy import inspect
 import json
-
+from sqlalchemy import create_engine
+from constants import ConnectionString
 
 class DatabaseSchema:
     def __init__(self, databaseEngine):
         self.databaseEngine = databaseEngine
-
-
 
     def create_database_schema(self):
         inspector = inspect(self.databaseEngine)
@@ -14,43 +13,39 @@ class DatabaseSchema:
 
         schemas = inspector.get_schema_names()
         for schema_name in schemas:
+            database_schema[schema_name] = {}
             tables = inspector.get_table_names(schema=schema_name)
-            if not tables :
+            if not tables:
                 continue
             for table_name in tables:
                 # Her tablo için sütun bilgilerini alıyoruz
-
-                database_schema[table_name] = []
-                primary_keys = inspector.get_pk_constraint(table_name= table_name, schema=schema_name)
-                foreign_keys = inspector.get_foreign_keys(table_name= table_name, schema=schema_name)
-                columns = inspector.get_columns(table_name= table_name, schema=schema_name)
-                print(columns)
-                database_schema[table_name] = {
+                primary_keys = inspector.get_pk_constraint(table_name=table_name, schema=schema_name)
+                foreign_keys = inspector.get_foreign_keys(table_name=table_name, schema=schema_name)
+                columns = inspector.get_columns(table_name=table_name, schema=schema_name)
+                
+                database_schema[schema_name][table_name] = {
                     "columns": self.get_table_columns_json(columns),
                     "primary_keys": primary_keys["constrained_columns"],
                     "foreign_keys": foreign_keys
                 }
 
-                # JSON formatında dosyaya yaz
-                with open("database_schema.json", "w") as json_file:
-                    json.dump(database_schema, json_file, indent=4)
-
-
-
-
+        # JSON formatında dosyaya yaz
+        with open("database_schema.json", "w") as json_file:
+            json.dump(database_schema, json_file, indent=4)
 
     def get_table_columns_json(self, columns):
-        for column in columns:     
+        column_info_list = []
+        for column in columns:
             column_info = {
                 "name": column["name"],
                 "type": str(column["type"]),
                 "nullable": column["nullable"],
                 "default": column["default"]
             }
-        
-        return column_info
+            column_info_list.append(column_info)
+        return column_info_list
 
+engine = create_engine(ConnectionString)
 
-
-
-
+database = DatabaseSchema(engine)
+database.create_database_schema()
--- text-to_sql.py
+++ text-to_sql.py
@@ -1,76 +1,80 @@
+from operator import itemgetter
 from click import prompt
-from sqlalchemy import create_engine
+from sqlalchemy import Table, create_engine
 
 from constants import OPENAI_API_KEY, OPENAI_API_TYPE, ConnectionString
 from langchain_community.document_loaders.json_loader import JSONLoader
-from langchain_openai import OpenAI
+from langchain_openai import OpenAI, ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma.vectorstores import Chroma
-from langchain_core.vectorstores import VectorStoreRetriever 
+from langchain_core.vectorstores import VectorStoreRetriever
 from langchain.prompts import PromptTemplate
 from langchain_experimental.sql.base import SQLDatabaseChain
-from langchain_experimental.sql.base import  SQLDatabase
+from langchain_experimental.sql.base import SQLDatabase
 from langchain_core.prompts.chat import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import RunnablePassthrough, graph_ascii, graph
+from langchain.chains.sql_database.query import create_sql_query_chain
 
+# OpenAI GPT-4 LLM configuration
+llm = ChatOpenAI(model_name="gpt-4", temperature=0, openai_api_key=OPENAI_API_KEY)
 
-# OpenAI GPT-4 LLM yapılandırması
-llm = OpenAI(model_name="gpt-4", temperature=0, openai_api_key=OPENAI_API_KEY)
+# Define the database
+db = SQLDatabase.from_uri(database_uri=ConnectionString)
 
-# Veritabanını tanımlayın
-db = SQLDatabase.from_uri(database_uri=ConnectionString)  
-
+# Define a text-to-SQL prompt template with instructions
 text_to_sql_prompt = ChatPromptTemplate.from_messages(
     [
         ("system",
-         "You are a SQL SERVER expert. Given an input question, create a syntactically correct PostgreSQL query to run and return ONLY the generated Query and nothing else. Unless otherwise specified, do not return more than \
-        {top_k} rows.\n\nHere is the relevant table info: {table_info}\
-        Pay close attention on which column is in which table. if context contains more than one tables then create a query by performing JOIN operation only using the column unitid for the tables.\
-        Follow these Instructions for creating syntactically correct SQL query:\
-        - Be sure not to query for columns that do not exist in the tables and use alias only where required.\
-        - Always use the column 'instnm' associated with the 'unitid' in the generated query.\
-        - Whenever asked for Institute Names, return the institute names using column 'instnm' associated with the 'unitid' in the generated query.\
-        - Likewise, when asked about the average (AVG function) or ratio, ensure the appropriate aggregation function is used.\
-        - Pay close attention to the filtering criteria mentioned in the question and incorporate them using the WHERE clause in your SQL query.\
-        - If the question involves multiple conditions, use logical operators such as AND, OR to combine them effectively.\
-        - When dealing with date or timestamp columns, use appropriate date functions (e.g., DATE_PART, EXTRACT) for extracting specific parts of the date or performing date arithmetic.\
-        - If the question involves grouping of data (e.g., finding totals or averages for different categories), use the GROUP BY clause along with appropriate aggregate functions.\
-        - Consider using aliases for tables and columns to improve readability of the query, especially in case of complex joins or subqueries.\
-        - If necessary, use subqueries or common table expressions (CTEs) to break down the problem into smaller, more manageable parts."),
+         "You are a T-SQL expert. Given an input question, create a syntactically correct MSSQL query to run and return ONLY the generated Query and nothing else. Unless otherwise specified, do not return more than \
+        {top_k} rows.\n\nHere is the relevant table info: {table_info}\n\n"
+         "Instructions for creating syntactically correct SQL query:\n"
+         "- Avoid using backticks. Use square brackets for identifiers.\n"
+         "- Always use the column 'instnm' associated with the 'unitid'.\n"
+         "- For institute names, use 'instnm' with 'unitid'.\n"
+         "- Use AVG and other aggregate functions as required.\n"
+         "- Apply filtering, GROUP BY with aggregation as needed.\n"
+         "- Use readable aliases where necessary.\n"
+         "- Return SQL code only in brackets or other delimiters."
+         ),
         MessagesPlaceholder(variable_name="messages"),
         ("human", "{input}"),
     ]
 )
 
-
-# Embedding işlemi için OpenAI kullanılıyor
+# Embedding using OpenAI
 openai_embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY, model="text-embedding-ada-002")
 
-# Veritabanı şemasını JSON dosyasından yükle
+# Load database schema from JSON file
 loader = JSONLoader(
-    file_path='database_schema.json',
+    file_path='schooldb.json',
     jq_schema='.',
     text_content=False)
 data = loader.load()
 vectorstore = Chroma.from_documents(data, openai_embeddings)
 retriever = vectorstore.as_retriever()
+
+# Define the retriever prompt
 template = """Answer the question based only on the following context:
     {context}
-    Search for the table descriptions in the context and accordingly search for column names and associated column description. Include only relevant tables and columns which can be used by the downstream Text-to-SQL Agent to create SQL Queries for generating answer.
-    Search for any information performing the following tasks:
+    Search for the table descriptions in the context and search for column names and associated column descriptions. Only include relevant tables and columns to create SQL Queries.
+    Tasks:
     1. Table Names
     2. Table Descriptions
     3. Column Names
     4. Column Descriptions
-    5. Encoded Values
-    Finally, only return table names, column names and Encoded Values only (if availabe).
+    5. Encoded Values (if available)
 
     Question: {question}
     """
 retriever_prompt = ChatPromptTemplate.from_template(template)
-retriever_chain = (
-    {"context": retriever, "question": RunnablePassthrough()}
+retriever_chain = retriever_prompt.format_prompt(
+    context=retriever, 
+    question="her eğitmenin verdiği dersleri listele"
 )
+
+# Initialize the SQL database chain
 db_chain = SQLDatabaseChain.from_llm(llm=llm, db=db)
 
-db_chain.invoke({"query": retriever_chain})
+# Run the chain and print results
+result = db_chain.invoke({"query": retriever_chain.to_string()})
+print(result["result"])
